515b4255ab5794eb1a068a924af8e0cb7f55481d
Kevin J. Hanna <kevinjackhanna@gmail.com>
2013-05-26 21:55:38 -0300

    Allow to view edit form for readings

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,7 @@
 angular.module('glucoseApp', ['glucoseApp.filters', 'glucoseApp.services', 'glucoseApp.directives', 'glucoseApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/readings', {templateUrl: 'partials/readings.html', controller: 'ReadingsCtrl'});
+    $routeProvider.when('/new/:readingId', {templateUrl: 'partials/new_reading.html', controller: 'NewReadingCtrl'});
     $routeProvider.when('/new', {templateUrl: 'partials/new_reading.html', controller: 'NewReadingCtrl'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/readings'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,11 +5,21 @@
 angular.module('glucoseApp.controllers', []).
   controller('ReadingsCtrl', ['$scope', 'Reading', function($scope, Reading) {
     $scope.readings = Reading.query();
-
   }])
-  .controller('NewReadingCtrl', ['$scope', 'Reading', function($scope, Reading) {
+  .controller('NewReadingCtrl', ['$scope', '$routeParams', 'Reading', function($scope, $routeParams, Reading) {
+    if ($routeParams.readingId)
+  {
+      $scope.reading = Reading.get({readingId: $routeParams.readingId}, function(reading) {
+      });
+  }
+
+    $scope.moments = [
+      {"label": "Morning (fasting)", "value": "morning"},
+      {"label": "Midday (2h after lunch)", "value": "night"},
+      {"label": "Night (2h after dinner)", "value": "night"}
+    ];
 
     $scope.save = function() {
-      console.log($scope.reading);
+      alert($scope.reading.moment);
     }
   }]);
